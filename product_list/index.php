615216488bcbf187c1b41d4511a4028d031c1ad2
Haoran Wei <63807711+Haoran101@users.noreply.github.com>
2021-09-28 03:52:49 +0800

    product list filter init

---
+++ product_list/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Product List</title>
+<meta charset="utf-8">
+</head>
+<body>
+    <!--navbar-->
+    <div class="filters">
+        <form id="all_filters" action="filtered.php" method="get">
+        <div class="brand-filters">
+            Brand 
+            <input type="checkbox" name="brand[]" id="brand1" value="brand1">
+            <label for="brand1">brand1</label>
+            <input type="checkbox" name="brand[]" id="brand2" value="brand2">
+            <label for="brand2">brand2</label>
+            <input type="checkbox" name="brand[]" id="brand3" value="brand3">
+            <label for="brand3">brand3</label>
+            <input type="checkbox" name="brand[]" id="brand4" value="brand4">
+            <label for="brand4">brand4</label>
+            <input type="checkbox" name="brand[]" id="brand5" value="brand5">
+            <label for="brand5">brand5</label>
+        </div>
+        <div class="type-filters">
+            type 
+            <input type="checkbox" name="type[]" id="type1" value="type1">
+            <label for="type1">type1</label>
+            <input type="checkbox" name="type[]" id="type2" value="type2">
+            <label for="type2">type2</label>
+            <input type="checkbox" name="type[]" id="type3" value="type3">
+            <label for="type3">type3</label>
+            <input type="checkbox" name="type[]" id="type4" value="type4">
+            <label for="type4">type4</label>
+            <input type="checkbox" name="type[]" id="type5" value="type5">
+            <label for="type5">type5</label>
+        </div>
+        <div class="price-filter">
+            Price
+            $<input type="text" name="min_price" id="min_price" size="2" value="0" onchange="validateNumberInput(this);">~
+            $<input type="text" name="max_price" id="max_price" size="2" value="1000" onchange="validateNumberInput(this);">
+            <div id="alert-msg"></div>
+        </div>
+        <input type="submit" value="Apply Filters">
+        </form>
+    </div>
+    <script src="product_list.js"></script>
+</body>
+</html>
---
+++ product_list/product_list.js
@@ -0,0 +1,17 @@
+function validateNumberInput(new_input){
+    var isdecimal = new_input.value.search(/^[+-]?([0-9]+\.?[0-9]*|\.[0-9]+)$/); 
+    if (isdecimal != 0){
+        document.getElementById("alert-msg").innerHTML = "Not a valid number input, please try again! ";
+        return;
+    } else {
+        document.getElementById("alert-msg").innerHTML = "";
+    }
+    
+        if (parseFloat(new_input.value)<0){
+            if (new_input.id == "min_price"){
+                new_input.value = 0;
+            } else {
+                new_input.value = 1000;
+            }
+    }
+}
