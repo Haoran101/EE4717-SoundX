f5d7419489d2d8b0fe2f8aabb79cadfcd0509ed3
ssshirin <sining040@gmail.com>
2021-10-01 02:24:38 +0800

    complete the style for product details

--- product_details/index.php
+++ product_details/index.php
@@ -7,80 +7,83 @@ $added_in_cart = in_array($product_id, $_SESSION['cart']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Details</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="../css/elements.css" />
-        <link rel="stylesheet" href="productdetails.css">
-    </head>
+
+<head>
+    <title>Details</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/elements.css" />
+    <link rel="stylesheet" href="../css/product_detail.css">
+</head>
+
 <body>
     <!-- navigation bar -->
     <?php include '../Elements/nav_bar.php';?>
-    <div class="product-title">
+    <div class="product-show">
         <div class="image-and-selector">
-            <img src=<?php echo"../img/product/{$product_id}-1.jpg"?> id="product_big_picture"/><br>
+            <div class="main-img">
+                <img src=<?php echo"../img/product/{$product_id}-1.jpg"?> id="product_big_picture" />
+            </div>
             <div class="image-selector-small">
-            <?php $num_of_pictures = (int) $row["no_pictures"];
+                <?php $num_of_pictures = (int) $row["no_pictures"];
             for ($pic = 1; $pic <= $num_of_pictures; $pic++){
                 $pic_source = "../img/product/".$product_id."-".$pic.".jpg";
-                echo '<img class="pic-thumbnail" src='.$pic_source.' id="product_pic_'.$pic.'" width="60px" height="60px" onclick="switchImage(\'product_pic_'.$pic.'\');"/>';
+                echo '<div class="image-selector-small-item">';
+                echo '<img class="pic-thumbnail" src='.$pic_source.' id="product_pic_'.$pic.'" width="60px" height="60px" onclick="switchImage(\'product_pic_'.$pic.'\');"/></div>';
             }?>
             </div>
         </div>
         <div class="text-title-part">
-                <h1><?php echo $row["product_name"]; ?></h1>
-                <h4><?php echo $row["short_description"]; ?></h4>
-                <p class="price"><strong>$<?php echo $row["price"]; ?></strong></p>
-                <form action="add_to_cart.php" method="post">
-                <input type="text" hidden name="product_id" value="<?php echo $product_id; ?>">
-                <button id="cart-button" onclick="this.form.submit();">
-                <?php
+            <h1><?php echo $row["product_name"]; ?></h1>
+            <h2><?php echo $row["short_description"]; ?></h2>
+            <p class="price"><strong>$<?php echo $row["price"]; ?></strong></p>
+            <div class="button-group">
+                <div id="add-to-cart-button">
+                    <form action="add_to_cart.php" method="post">
+                        <input type="text" hidden name="product_id" value="<?php echo $product_id; ?>">
+                        <button id="cart-button" onclick="this.form.submit();">
+                            <?php
                     if ($added_in_cart){
                         echo "View In Cart";
                     } else {echo "Add to Cart";}
                 ?>
-                </button>
-                </form>
-                <button id="buy-now-button">Buy Now</button>
+                        </button>
+                    </form>
+                </div>
+                <div id="buy-now-button">
+                    <button id="buy-button">Buy Now</button>
+                </div>
+            </div>
+
         </div>
     </div>
-    <br>
-    <hr class="solid">
-    <div class="Description">
-        <h1>Description</h1>
-        <p><?php echo $row["description"]; ?></p>
     </div>
-    <br>
-    <hr class="solid">
-    <div class="Spec">
-        <h1>Specification</h1>
-        <table id="spec-table">
-        <?php $json_content = json_decode($row["specification"], true);
+
+
+    <div class="product-detail-info">
+        <div class="detail-info-container">
+            <h1>Description</h1>
+            <hr class="solid">
+            <p><?php echo $row["description"]; ?></p>
+        </div>
+
+        <div class="detail-info-container">
+            <h1>Specification</h1>
+            <hr class="solid">
+            <table id="spec-table">
+                <?php $json_content = json_decode($row["specification"], true);
                 foreach($json_content as $key => $value){
                     echo "<tr class='spec-table'>";
-                    echo "<td class='spec-table'>".$key."</td>";
-                    echo "<td class='spec-table'>".$value."</td>";
+                    echo "<td class='spec-table-key'>".$key."</td>";
+                    echo "<td class='spec-table-value'>".$value."</td>";
                     echo "</tr>";
                 }
-        ?>
-        </table>
-    </div>
-    <br>
-    <hr class="solid">
-    <div>
-        <h1>See More</h1>
-        <a class="product-card-link" href="">
-        <div class="product-card" id="product_id_1">
-            <div class="centered">
-            <img class="product-small-pic" src="../img/product-snapshot/1.png" width="150px">
-            <h3>PlaceHolder Product Name</h3>
-            <h1>$169.90</h1>
-            </div>
-        </a>
-                
+                ?>
+            </table>
         </div>
+    </div>
     </div>
     <?php include '../Elements/footer.php'; ?>
     <script type="text/javascript" src="productdetails.js"></script>
 </body>
+
 </html>
