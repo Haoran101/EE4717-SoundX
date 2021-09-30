4972b9f927f7e4569c15271456e6f5aa1ed1f25f
Haoran101 <whr991001@gmail.com>
2021-09-30 21:48:21 +0800

    convert to php

--- Elements/nav_bar.php
+++ Elements/nav_bar.php
@@ -1,14 +1,19 @@
+<?php include_once '../db_conn.php';
+
+$brands = array('Razer', 'Beats', 'Sony', 'SteelSeries', 'Bose');
+$types = array('Wired', 'Wireless', 'Gaming', 'Sport');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>SoundX</title>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="../css/nav-bar-style.css" />
   </head>
   <body>
 <div class="nav-bar-container">
         <!-- Left part -->
-        <a class="logo" href="index.html">
+        <a class="logo" href="../home/">
           <img src="../img/SoundX-Logo.png" alt="SoundX" width="100px">
         </a>
         <div class="brand-container">
@@ -20,41 +25,41 @@
 			<ul class="nav-dropdown-item">
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href="../product_list/filtered.php?brand%5B%5D=1&min_price=0&max_price=1000">
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Brand 1</div>
+							<div class="font-item"><?php echo $brands[0]; ?></div>
 						</a>						
 					</div>
 				</li>
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href="../product_list/filtered.php?brand%5B%5D=2&min_price=0&max_price=1000">
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Brand 1</div>
+							<div class="font-item"><?php echo $brands[1]; ?></div>
 						</a>					
 					</div>
 				</li>
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href="../product_list/filtered.php?brand%5B%5D=3&min_price=0&max_price=1000">
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Very long name of Brand 1</div>
+							<div class="font-item"><?php echo $brands[2]; ?></div>
 						</a>						
 					</div>
 				</li>
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href="../product_list/filtered.php?brand%5B%5D=4&min_price=0&max_price=1000">
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Brand 1</div>
+							<div class="font-item"><?php echo $brands[3]; ?></div>
 						</a>						
 					</div>
 				</li>
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href="../product_list/filtered.php?brand%5B%5D=5&min_price=0&max_price=1000">
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Brand 1</div>
+							<div class="font-item"><?php echo $brands[4]; ?></div>
 						</a>					
 					</div>
 				</li>
@@ -70,43 +75,43 @@
 			<ul class="nav-dropdown-item">
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href=<?php echo "../product_list/filtered.php?{$types[0]}=true&min_price=0&max_price=1000"; ?>>
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Brand 1</div>
+							<div class="font-item"><?php echo $types[0]; ?></div>
 						</a>						
 					</div>
 				</li>
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href=<?php echo "../product_list/filtered.php?{$types[1]}=true&min_price=0&max_price=1000"; ?>>
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Brand 1</div>
+							<div class="font-item"><?php echo $types[1]; ?></div>
 						</a>					
 					</div>
 				</li>
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href=<?php echo "../product_list/filtered.php?{$types[2]}=true&min_price=0&max_price=1000"; ?>>
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Very long name of Brand 1</div>
+							<div class="font-item"><?php echo $types[2]; ?></div>
 						</a>						
 					</div>
 				</li>
 				<li class="subnav-item">
 					<div class="item-block">
-						<a class="item-block-link" href="brand.html">
+						<a class="item-block-link" href=<?php echo "../product_list/filtered.php?{$types[3]}=true&min_price=0&max_price=1000"; ?>>
 							<img src="../img/placeholder.png" alt="SoundX" width="130px"><br>
-							<div class="font-item">Brand 1</div>
+							<div class="font-item"><?php echo $types[3]; ?></div>
 						</a>						
 					</div>
 				</li>
 			</ul>
           </div>
         </div>
-        <a class="left-nav" href="product_list/index.php">Product</a>
+        <a class="left-nav" href="../product_list/">Product</a>
         <a class="left-nav" href="#contact">Contact</a>
         <!-- Right part -->
-        <a class="right-nav" href="html/accountinfo.html">
+        <a class="right-nav" href="../account/">
         <img class="account-logo" src="../img/profile-user.png" alt="account"></a>
         <a class="right-nav" href="html/cart.html">
         <img class="cart-logo" src="../img/shopping-cart.png" alt="cart" style="padding-top: 3px;"></a>
