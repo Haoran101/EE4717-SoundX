8bd11db148d3e945aaf5d1550fb23c08d23fca27
ssshirin <sining040@gmail.com>
2021-10-02 01:02:04 +0800

    complete shopping cart styling

--- cart/index.php
+++ cart/index.php
@@ -15,7 +15,7 @@
 <body>
     <?php include '../Elements/nav_bar.php';?>
     <div class="item-cart-title">
-        <h1>Items</h1>
+        <h1>MY CART</h1>
     </div>
     <?php 
     if (count($_SESSION['cart'])==0){
@@ -27,24 +27,26 @@
         foreach ($_SESSION['cart'] as $product){
             $info = get_details_by_id($db, $product);
             echo "<tr><td>";
-            echo '<div class="cart-item-row">';
+            echo '<div class="cart-row">';
             echo "<input type='checkbox' name='selected[]' value='{$product}' id='cart-checkbox'>";
-            echo "<img src='../img/product-snapshot/{$product}.png' id='cart-img'>";
+            echo "<div class='cart-img-container'>";
+            echo "<img src='../img/product-snapshot/{$product}.png' id='cart-img'></div>";
             echo '<div id="product_name_quan">';
-            echo '<p>'.$info['product_name']'</p>';
+            echo '<p>'.$info['product_name'].'</p>';
             echo '<div id="quan_field">';
+            echo '<label for="brand2">Quantity</label>';
             echo "<input type='number' name='items[{$product}]' value='1'>";
             echo '</div></div>';
-            echo '<div>';
-            echo $info['price'];
+            echo '<div id="price-field">';
+            echo '<p>'.'$'.$info['price'].'</p>';
             //echo "<input type='number' name='items[{$product}]' value='1'>";
-            echo 'M./div></div>';
+            echo '</div></div>';
             echo "</td></tr>";
         }
         echo "</table>";
         echo '<input type="submit" value="Checkout">';
         echo '</form>';
-        echo '</div>'
+        echo '</div>';
     }
     ?>
     <?php include '../Elements/footer.php';?>
