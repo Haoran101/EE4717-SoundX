e9ae830cbef842a4efeebfb84f40f099a5c3e43d
Haoran101 <whr991001@gmail.com>
2021-10-15 00:22:09 +0800

    sign up form validation

--- sign_up/signup.js
+++ sign_up/signup.js
@@ -1,14 +1,50 @@
+var signUpButton = document.getElementById("signup-button");
 var passwordField = document.getElementById('Password');
 var confirmPasswordField = document.getElementById('ConfirmPassword');
+var contactField = document.getElementById("Contact");
+var emailField = document.getElementById("Email");
+
 passwordField.addEventListener("change", confirmPassword);
 confirmPasswordField.addEventListener("change", confirmPassword);
+contactField.addEventListener("change", validateContactNumber);
+emailField.addEventListener("change", validateEmail);
+
+function validateContactNumber(){
+    let contact = contactField.value;
+    let alert = document.getElementById("invalidContactAlert");
+    if (contact.search(/^\d{8}$/) !== 0){
+        alert.innerHTML = "A valid phone number contains 8 digits.";
+        signUpButton.disabled = true;
+    } else {
+        alert.innerHTML ="";
+        signUpButton.disabled = false;
+    }
+}
+
+function validateEmail(){
+    let email = emailField.value;
+    console.log(email);
+    let alert = document.getElementById("invalidEmailAlert");
+    if (/^[\w\.-]+@([\w]+\.){1,3}[\w]+$/.test(email)){
+        alert.innerHTML ="";
+        signUpButton.disabled = false;
+    } else {
+        alert.innerHTML = "Please Enter a valid email!";
+        signUpButton.disabled = true;
+    }
+}
 
 function confirmPassword(){
-    console.log('triggered');
     var password = document.getElementById('Password').value;
     var confirmedPassword = document.getElementById('ConfirmPassword').value;
-    if (password.length > 0 && confirmedPassword.length > 0 && confirmedPassword != password){
-        document.getElementById("notSamePasswordAlert").innerHTML = "Password inputs are not the same!";
+    let alert =  document.getElementById("notSamePasswordAlert");
+    if (password.length > 0 && confirmedPassword.length > 0){
+        if (confirmedPassword != password){
+            alert.innerHTML = "Password inputs are not the same!";
+            signUpButton.disabled = true;
+        } else {
+            alert.innerHTML = "";
+            signUpButton.disabled = false;
+        }
     }
-    return null;
 }
