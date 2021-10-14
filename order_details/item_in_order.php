50052764d763ec1da5d743b72fcc3801ff5132b8
ssshirin <sining040@gmail.com>
2021-10-14 23:09:46 +0800

    finish order page and order item styling

--- order_details/item_in_order.php
+++ order_details/item_in_order.php
@@ -1,16 +1,15 @@
 <?php 
 function order_item_table($order_id, $order_details, $order_items){
     $order_id = str_pad($order_id, 8, '0', STR_PAD_LEFT);
-    echo "<table class='order-table-main'>
-    <tr><td id='order-id-row'><!-- order id row -->
-        <p><strong>Order</strong>#{$order_id}</p>
-    </td></tr>
-    <tr><td id='create-time-row'><!-- create time row -->
+    echo "<div class='order-table-main'>
+    <div id='order-id-row'>
+        <p><strong>Order:</strong> #{$order_id}</p>
+    </div>
+    <div id='create-time-row'><!-- create time row -->
          <p><strong>Created Time:</strong> {$order_details['create_time']}</p>
-    </td></tr>";
+    </div>";
 
-    echo "<tr><td><!-- items row, contains items table -->
-    <div class='item-row-container'>
+    echo "<div class='item-row-container'><!-- items row, contains items table -->
     <table>
         <tr>
             <td colspan='2' id='table-head-item'>Item</td>
@@ -20,25 +19,20 @@ function order_item_table($order_id, $order_details, $order_items){
         </tr>";
     $total = 0;
     foreach($order_items as $item){
-        echo "<tr><div class='item-row'";
-        echo "<td><div id='img-container'><img src='../img/product-snapshot/{$item['product_id']}.png' id='order-img'><div></td>";//image
-        echo "<td><div id='product-name-container'><p>{$item['product_name']}</p></div></td>";//product name
-        echo "<td><div id='qty-container'><p>{$item['qty']}</p></div></td>";//Qty
+        echo "<tr class='item-row'>";
+        echo "<td id='img-container'><img src='../img/product-snapshot/{$item['product_id']}.png' id='order-img'></td>";//image
+        echo "<td id='product-name-container'><p>{$item['product_name']}</p></td>";//product name
+        echo "<td id='qty-container'><p>{$item['qty']}</p></td>";//Qty
         $price =  sprintf('%0.2f', $item['price']);
-        echo "<td><div id='price-container'><p>\${$price}</p></div></td>";//Price
+        echo "<td id='price-container'><p>\${$price}</p></td>";//Price
         $subtotal = sprintf('%0.2f', $item['qty'] * $item['price']);
         $total += $item['qty'] * $item['price'];
-        echo "<td><div id='subtotal-container'><p>\${$subtotal}</p></div></td>";//Subtotal
+        echo "<td id='subtotal-container'><p>\${$subtotal}</p></td>";//Subtotal
         echo "</tr>";
     }
-    echo "</table>
-    </div>
-    </td></tr>
-
-    <tr id='total-row'><td id='total'><!-- total row -->
-        <p><strong>Total</stromg></p></td>"; 
-        echo "<td id='total-amount'>\$";
-        echo sprintf('%0.2f', $total);
-    echo "</td></tr></table>";
+    echo "<tr class='total-row'><td colspan='4' id='total'><p>Total</p></td>"; 
+    echo "<td id='total-amount'><p>\$";
+    echo sprintf('%0.2f', $total);
+    echo "</p></td></tr></table></div>";
 }
 ?>
